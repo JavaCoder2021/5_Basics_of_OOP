75b004567267f7b9a71818631514c716738d8b53
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-23 13:59:07 +0200

    Create Customer.java

---
+++ task2/Customer.java
@@ -0,0 +1,17 @@
+package by.epam.basics_of_opp;
+
+public class Customer {
+    
+    private String name;
+        
+    public Customer(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getCustomerName()
+    {
+        return name;
+    }
+    
+}
