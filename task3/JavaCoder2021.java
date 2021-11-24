8d6c96949c1c0a2a0bc57a8531db7cff163315c1
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-24 15:05:51 +0200

    Update JavaCoder2021.java

--- task3/JavaCoder2021.java
+++ task3/JavaCoder2021.java
@@ -1,3 +1,9 @@
+/*
+ * Задача 3.
+ * Создать класс Календарь с внутренним классом,
+ * с помощью объектов которого можно хранить
+ * информацию о выходных и праздничных днях.
+ */
 package javacoder2021;
 
 import by.epam.basics_of_oop.task3.*;
@@ -17,20 +23,7 @@ public class JavaCoder2021 {
         calendar.addHoliday(MonthDay.of(Month.MAY, 9));
         calendar.addHoliday(MonthDay.of(Month.NOVEMBER, 7));
 
-        calendar.showWeekendAndHolidayBetween(LocalDate.of(2021, 1, 1), LocalDate.of(2022, 1, 1));        
-        
-        //strings_as_object
-        //by.epam.strings.strings_as_object.Task1.main(args);
-        //by.epam.strings.strings_as_object.Task2.main(args);
-        //by.epam.strings.strings_as_object.Task2i.main(args);
-        //by.epam.strings.strings_as_object.Task3.main(args);
-        //by.epam.strings.strings_as_object.Task4.main(args);
-        //by.epam.strings.strings_as_object.Task5.main(args);
-        //by.epam.strings.strings_as_object.Task6.main(args);
-        //by.epam.strings.strings_as_object.Task7.main(args);
-        //by.epam.strings.strings_as_object.Task8.main(args);
-        //by.epam.strings.strings_as_object.Task9.main(args);
-        //by.epam.strings.strings_as_object.Task10.main(args);
+        calendar.showWeekendAndHolidayBetween(LocalDate.of(2021, 1, 1), LocalDate.of(2022, 1, 1));
         
     }
     
