d590c3b167470ad08534c2df50d9dc1b7f498c9a
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-24 18:15:34 +0200

    Update TextFileReader.java

--- task1/TextFileReader.java
+++ task1/TextFileReader.java
@@ -1,4 +1,4 @@
-package by.epam.basics_of_oop;
+package by.epam.basics_of_oop.task1;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,14 +11,12 @@ public class TextFileReader {
     
     public File createFile(String fileName) {
         File newFile = new File(fileName);
-        try
-        {
+        try {
             boolean created = newFile.createNewFile();
             if(created)
                 System.out.println("File has been created!");
         }
-        catch(IOException ex)
-        {
+        catch(IOException ex) {
              
             System.out.println(ex.getMessage());
         } 
@@ -27,26 +25,22 @@ public class TextFileReader {
     
     public File renameFile(File myFile, String filename) {
         File newFile = new File(filename);
-        if (myFile.renameTo(newFile))
-        {
+        if (myFile.renameTo(newFile)) {
             System.out.println("File renamed successfully!");;
         }
-        else
-        {
+        else {
             System.out.println("The file has not been renamed!");
         }
         return newFile;
     }
     
     public void appendFile(File myFile, String str) {   
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(myFile, true)))
-        {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(myFile, true))) {
             String updatedText = str.replaceAll("\n", System.lineSeparator());
             writer.write(updatedText);
             writer.close();
         }
-        catch ( IOException e )
-        {
+        catch ( IOException e ) {
             System.out.println(e.getMessage());
         }   
     }
@@ -54,27 +48,23 @@ public class TextFileReader {
     public void readFile(File myFile) {      
         String lineRead;
         System.out.println("---------------------");                
-        try (BufferedReader reader = new BufferedReader(new FileReader(myFile)))
-        {
+        try (BufferedReader reader = new BufferedReader(new FileReader(myFile))) {
             
             lineRead = reader.readLine();
-            while (lineRead != null)
-            {
+            while (lineRead != null) {
                 System.out.println(lineRead);
                 lineRead = reader.readLine();
             }
             reader.close();
         }    
-        catch (IOException e)
-        {
+        catch (IOException e) {
             System.out.println(e.getMessage());
         }
         System.out.println("---------------------"); 
     }
 
     public void deleteFile(File myFile) {     
-        if(myFile.delete())
-        {
+        if(myFile.delete()) {
             System.out.println(myFile.getName() + " the file has been deleted!");
         }
         else 
