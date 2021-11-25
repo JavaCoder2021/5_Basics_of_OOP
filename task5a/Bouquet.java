ebdb3cd074795d1860531f63444b4553e7998cc2
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-25 23:03:39 +0200

    Create Bouquet.java

---
+++ task5a/Bouquet.java
@@ -0,0 +1,50 @@
+package by.epam.basics_of_oop.task5a;
+
+import java.util.*;
+
+/**
+ *
+ * @author Igor
+ */
+public class Bouquet {
+    
+    public static enum Package {
+        Paper,
+        Film,
+        Net,
+        Organza,
+        Felt,
+        Foamiran,
+        Sisal,
+        Crumbles;
+    }
+    
+    private Package pack; 
+    private List<Flower> flowers = new ArrayList<Flower>();
+    
+    public Bouquet(Flower flower, Package pack) {
+        if (flower == null) {
+            throw new NullPointerException("Argument flower can not be null.");
+        }
+        this.flowers.add(flower);
+        if (pack == null) {
+            throw new NullPointerException("Argument pack can not be null.");
+        }
+        this.pack = pack;
+    }
+        
+    public void addFlower(Flower flower) {
+        flowers.add(flower);
+    }
+    
+    @Override
+    public String toString() {
+        String str = "Bouquet:\n";
+        for(Flower flower : flowers)
+           str += "Flower: " + flower.getName() + ";\t Color: " + 
+                   flower.getColor() + ";\n";
+        str += "Package: " + pack;
+        return str;
+    }
+    
+}
