ca536a789e14bc8fd16f1c4a3875556d129d2ba1
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-25 23:06:15 +0200

    Create Tulip.java

---
+++ task5a/Tulip.java
@@ -0,0 +1,13 @@
+package by.epam.basics_of_oop.task5a;
+
+/**
+ *
+ * @author Igor
+ */
+public class Tulip extends Flower{
+    
+    public Tulip(Flower.Color color) {
+        super("Tulip", color);
+    }
+    
+}
