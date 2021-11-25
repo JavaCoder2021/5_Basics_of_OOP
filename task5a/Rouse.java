a55a89b94cdb0ec586f80e7dcd7a2924a7aaadd0
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-25 23:05:19 +0200

    Create Rouse.java

---
+++ task5a/Rouse.java
@@ -0,0 +1,13 @@
+package by.epam.basics_of_oop.task5a;
+
+/**
+ *
+ * @author Igor
+ */
+public class Rouse extends Flower{
+    
+    public Rouse(Flower.Color color) {
+        super("Rouse", color);
+    }
+    
+}
