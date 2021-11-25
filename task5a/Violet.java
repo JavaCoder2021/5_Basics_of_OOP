8504a96a20e30a44a776040c67467f69937bc140
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-25 23:05:48 +0200

    Create Violet.java

---
+++ task5a/Violet.java
@@ -0,0 +1,13 @@
+package by.epam.basics_of_oop.task5a;
+
+/**
+ *
+ * @author Igor
+ */
+public class Violet extends Flower{
+    
+    public Violet(Flower.Color color) {
+        super("Violet", color);
+    }
+    
+}
