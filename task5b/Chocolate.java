f3246482c8f3a90a731bd98b4ce5441acfe98940
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-26 21:44:05 +0200

    Create Chocolate.java

---
+++ task5b/Chocolate.java
@@ -0,0 +1,13 @@
+package by.epam.basics_of_oop.task5b;
+
+/**
+ *
+ * @author Igor
+ */
+public class Chocolate extends Sweetness {
+    
+    public Chocolate(Sweetness.Manufacturer manufacturer) {
+        super("Chocolate", manufacturer);
+    }
+    
+}
