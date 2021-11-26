f9314d4f4eddb830211155385641b0fbcf279c5a
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-26 21:42:19 +0200

    Create Wafer.java

---
+++ task5b/Wafer.java
@@ -0,0 +1,13 @@
+package by.epam.basics_of_oop.task5b;
+
+/**
+ *
+ * @author Igor
+ */
+public class Wafer extends Sweetness {
+    
+    public Wafer(Sweetness.Manufacturer manufacturer) {
+        super("Wafer", manufacturer);
+    }
+    
+}
