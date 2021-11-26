924db96c61310abb0f0afed23cdcd81ebf1180c7
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-26 19:17:58 +0200

    Create Rose.java

---
+++ task5a/Rose.java
@@ -0,0 +1,13 @@
+package by.epam.basics_of_oop.task5a;
+
+/**
+ *
+ * @author Igor
+ */
+public class Rose extends Flower{
+    
+    public Rose(Flower.Color color) {
+        super("Rose", color);
+    }
+    
+}
