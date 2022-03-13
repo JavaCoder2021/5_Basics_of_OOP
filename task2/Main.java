b02ed2577e57aa7c3cfb836cbc76b4ae1fa866fe
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-13 15:58:27 +0200

    Update Main.java

--- task2/Main.java
+++ task2/Main.java
@@ -1,34 +1,18 @@
-/*
- * Задача 2.  
- * Создать класс Payment с внутренним классом, 
- * с помощью объектов которого можно сформировать покупку из нескольких товаров. 
- */
 package by.epam.basics_of_oop.task2;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        
-        Customer andrew = new Customer("Andrew");
-        Customer peter  = new Customer("Peter"); 
-             
-        Payment andrewPayment = new Payment(andrew);
-        Payment peterPayment = new Payment(peter);
-        
-        andrewPayment.buyApple();
-        andrewPayment.buyGarnet();
-        andrewPayment.buyLemon();
-        andrewPayment.buyLemon();
-        andrewPayment.getTotalSum();
-        
-        System.out.println("-------------------------------");
-        
-        peterPayment.buyOrange();
-        peterPayment.buyOrange();
-        peterPayment.buyPear();
-        peterPayment.buyPear();
-        peterPayment.getTotalSum();        
-        
-    }
-    
+
+	public static void main(String[] args) {
+
+		Payment payment = new Payment();
+		
+		payment.addProduct(new Payment.Product("Orange", 500.0));
+		payment.addProduct(new Payment.Product("Bananas", 700.0));
+		
+		PaymentShow.allProducts(payment.getProducts());
+		
+		System.out.println("Sum=" + PaymentLogic.getSum(payment.getProducts()));
+
+	}
+
 }
