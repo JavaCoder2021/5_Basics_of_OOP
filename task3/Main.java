62218ec7143f1e9c92dc330d462f748cabea4e16
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-05-03 19:35:27 +0200

    Update Main.java

--- task3/Main.java
+++ task3/Main.java
@@ -6,14 +6,12 @@
  */
 package by.epam.basics_of_oop.task3;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.MonthDay;
+import java.time.*;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        
+
+	public static void main(String[] args) {
+		
         Calendar calendar = new Calendar();
 
         calendar.addHoliday(MonthDay.of(Month.JANUARY, 1));
@@ -22,8 +20,8 @@ public class Main {
         calendar.addHoliday(MonthDay.of(Month.MAY, 9));
         calendar.addHoliday(MonthDay.of(Month.NOVEMBER, 7));
 
-        calendar.showWeekendAndHolidayBetween(LocalDate.of(2021, 1, 1), LocalDate.of(2022, 1, 1));
+        calendar.showWeekendAndHolidayBetween(LocalDate.of(2022, 1, 1), LocalDate.of(2022, 6, 1));
         
-    }
-    
+	}
+
 }
