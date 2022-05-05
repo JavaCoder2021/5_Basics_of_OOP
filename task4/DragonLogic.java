c200c07b840f2491799c8f5fd949b012d67c0e5e
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-05-05 21:25:17 +0200

    Update DragonLogic.java

--- task4/DragonLogic.java
+++ task4/DragonLogic.java
@@ -6,12 +6,12 @@ import by.epam.basics_of_oop.task4.bean.Dragon;
 import by.epam.basics_of_oop.task4.bean.Treasure;
 
 public class DragonLogic {
-	
+
 	private Random random = new Random();
 	private int sum;
 	private String name;
 	private Dragon dragon;
-	
+
 	public DragonLogic(Dragon dragon) {
 		super();
 		this.dragon = dragon;
@@ -22,7 +22,7 @@ public class DragonLogic {
 			sum = random.nextInt(100) + 1;
 			name = String.valueOf(i);
 			dragon.addTreasure(new Treasure(name, sum));
-        }
+		}
 	}
 
 }
